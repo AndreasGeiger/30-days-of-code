9bf2a6308d77203d1d4f7e413a086cb7392e6d61
Andreas Geiger <geiger911@gmail.com>
2017-04-03 17:49:14 +0200

    Twentyeigth day

---
+++ 27 - Testing/SolutionDay27.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SolutionDay27 {
+    public static void main(String[] args) {
+        System.out.println(5);
+
+        System.out.println("4 3");
+        System.out.println("-1 0 1 2");
+
+        System.out.println("3 1");
+        System.out.println("-1 0 1");
+
+        System.out.println("5 4");
+        System.out.println("-1 0 1 2 3");
+
+        System.out.println("6 1");
+        System.out.println("-1 0 1 2 3 4");
+
+        System.out.println("7 6");
+        System.out.println("-1 0 1 2 3 4 5");
+    }
+}
